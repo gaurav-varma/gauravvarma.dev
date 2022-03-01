61afcaa3d1fb19f6e8ea499b815d1fc336b1ebe2
Gaurav Varma <gauravvarma.dev@gmail.com>
2022-03-01 12:54:22 +0530

    Add basic layout & css framework
    * Add bootstrap css.
    * Remove sample code, styles, images.
    * Disable no-img-element in eslint.
    * Add layout components.
    * Add new favicon.
    * Add name and dp on home page.

---
+++ layouts/header.js
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Header() {
+  return (
+    <header className="d-flex justify-content-center py-3">
+      <div className="py-5 text-center">
+        <img
+          className="d-block mx-auto mb-2 rounded-circle"
+          src="/gauravvarma.jpg"
+          alt="Picture of the author"
+          width={100}
+          height={100}
+          layout="fixed"
+        />
+        <p className={styles.name}>GAURAV VARMA</p>
+      </div>
+    </header>
+  );
+}
