0da81fb0d339b96b05ed49859d297bde1024fa75
gaurav-varma <gauravvarma.dev@gmail.com>
2022-03-02 17:56:06 +0530

    Add next-seo, icons, .webmanifest, robots.txt, browserconfig.xml
    * Add next-seo version v4.28.1.
    * Add DefaultSeo component and config to app.
    * Add meta, rel, description etc for SEO.
    * Add apple and android touch icons.
    * Add favicons with different sizes.
    * Add manifest.webmanifest with config.
    * Add browserconfig.xml with config.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,105 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { DefaultSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <DefaultSeo
+        title="Gaurav Varma"
+        description="I'm a Full Stack Ruby on Rails engineer."
+        canonical={"https://gauravvarma.dev"}
+        openGraph={{
+          site_name: "Gaurav Varma",
+          title: "Gaurav Varma",
+          description: "I'm a Full Stack Ruby on Rails engineer.",
+          images: [
+            {
+              url: "https://www.gauravvarma.dev/gauravvarma.jpg",
+              width: 100,
+              height: 100,
+              alt: "Gaurav Varma Ruby on Rails Developer",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@gauravvarmaa",
+          site: "@gauravvarmaa",
+          cardType: "summary_large_image",
+        }}
+        additionalMetaTags={[
+          {
+            name: "application-name",
+            content: "Gaurav Varma",
+          },
+          {
+            name: "msapplication-TileColor",
+            content: "#ffffff",
+          },
+          {
+            name: "theme-color",
+            content: "#ffffff",
+          },
+          {
+            name: "apple-mobile-web-app-title",
+            content: "Gaurav Varma",
+          },
+        ]}
+        additionalLinkTags={[
+          {
+            rel: "apple-touch-icon",
+            href: "/apple-touch-icon.png",
+            sizes: "180x180",
+          },
+          {
+            rel: "icon",
+            href: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            rel: "icon",
+            href: "/favicon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            rel: "manifest",
+            href: "/manifest.webmanifest",
+          },
+          {
+            rel: "mask-icon",
+            href: "/safari-pinned-tab.svg",
+            color: "#000000",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/touch-icons/apple-touch-icon-ipad-retina-152x152.png",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/touch-icons/apple-touch-icon-ipad-76x76.png",
+            sizes: "60x60",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/touch-icons/apple-touch-icon-ipad-retina-152x152.png",
+            sizes: "144x144",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/touch-icons/apple-touch-icon-iphone-60x60.png",
+            sizes: "60x60",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/touch-icons/apple-touch-icon-iphone-retina-120x120.png",
+            sizes: "114x114",
+          },
+        ]}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
