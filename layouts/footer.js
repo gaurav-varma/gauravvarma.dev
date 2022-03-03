ed861a208e07ecb4f7b160bfa7a9ecc8504a58e8
gaurav-varma <gauravvarma.dev@gmail.com>
2022-03-03 18:27:01 +0530

    Add footer with blog github link to home page.

--- layouts/footer.js
+++ layouts/footer.js
@@ -1,35 +1,17 @@
 export default function Footer() {
-    return (
-      <footer className="py-3 my-4">
-        <ul className="nav justify-content-center border-bottom pb-3 mb-3">
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-muted">
-              Home
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-muted">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-muted">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-muted">
-              FAQs
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#" className="nav-link px-2 text-muted">
-              About
-            </a>
-          </li>
-        </ul>
-        <p className="text-center text-muted">© 2021 Company, Inc</p>
-      </footer>
-    );
-  }
-  
+  return (
+    <footer className="py-3 my-4">
+      <p className="text-center text-muted fw-light">
+        The code for this blog is{" "}
+        <a
+          className="text-decoration-underline text-muted fw-light"
+          href="https://github.com/gaurav-varma/gauravvarma.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          available on Github
+        </a>
+      </p>
+    </footer>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
     <main>
       <Header></Header>
       <BlogList></BlogList>
+      <Footer></Footer>
     </main>
   );
 }
