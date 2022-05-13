411f0b7f4c5f1de31c0cb7638205801b7990aec7
gaurav-varma <gauravvarma.dev@gmail.com>
2022-05-13 16:00:06 +0530

    Update BlogList component to render from objects instead of static html.

--- components/blogList.js
+++ components/blogList.js
@@ -1,152 +1,55 @@
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
-export default function BlogList() {
+function OlderPostsLink(props) {
+  const showOlderPostLink = props.showOlderPostLink;
+  if (showOlderPostLink) {
+    return (
+      <div className="d-flex justify-content-end mb-4 mt-4">
+        <Link href="/blog">
+          <a className="text-uppercase text-decoration-none text-muted fw-light">
+            Older Posts →
+          </a>
+        </Link>
+      </div>
+    );
+  }
+  return null;
+}
+
+export default function BlogList({ posts, showOlderPostLink = false }) {
   return (
     <div className="container px-4 px-lg-5">
       <div className="row gx-4 gx-lg-5 justify-content-center">
         <div className="col-md-10 col-lg-8 col-xl-8 col-sm-12">
-          <div className="post-preview">
-            <a
-              href="https://www.bigbinary.com/blog/rails-7-adds-from-option-to-assert_no_changes"
-              target="_blank"
-              className={styles.post_title_link}
-              rel="noreferrer"
-            >
-              <h4 className="post-title fw-bold">
-                Rails 7 Adds from option to
-                ActiveSupport::TestCase#assert_no_changes
-              </h4>
-            </a>
-            <p className="post-subtitle">
-              Rails Active Support provides various extensions, utilities, and
-              helpers. It provides a collection of utility classes...
-            </p>
-            <p className="post-meta">November 19, 2021</p>
-          </div>
-          <hr className="my-4"></hr>
-          <div className="post-preview">
-            <a
-              href="https://www.bigbinary.com/blog/rails-7-replaced-byebug-with-ruby-debug"
-              target="_blank"
-              className={styles.post_title_link}
-              rel="noreferrer"
-            >
-              <h4 className="post-title fw-bold">
-                Rails 7 replaced byebug with ruby/debug
-              </h4>
-            </a>
-            <p className="post-subtitle ">
-              Rails 5 introduced byebug which is an easy-to-use, feature-rich
-              ruby debugger. It offers features like Stepping, Breaking,
-              Evaluating, Tracking...
-            </p>
-            <p className="post-meta">November 9, 2021</p>
-          </div>
-          <hr className="my-4"></hr>
-          <div className="post-preview">
-            <a
-              href="https://www.bigbinary.com/blog/rails-7-adds-weekday_options_for_select-and-weekday_select"
-              target="_blank"
-              className={styles.post_title_link}
-              rel="noreferrer"
-            >
-              <h4 className="post-title fw-bold">
-                Rails 7 adds weekday_options_for_select and weekday_select
-              </h4>
-            </a>
-            <p className="post-subtitle ">
-              In web applications, forms are one of the most essential
-              interfaces for user input and it can be tedious to write...
-            </p>
-            <p className="post-meta">November 9, 2021</p>
-          </div>
-          <hr className="my-4"></hr>
-          <div className="post-preview">
-            <a
-              href="https://www.bigbinary.com/blog/rails-7-adds-activerecord-previously_persisted"
-              target="_blank"
-              className={styles.post_title_link}
-              rel="noreferrer"
-            >
-              <h4 className="post-title fw-bold">
-                Rails 7 adds ActiveRecord::Base#previously_persisted?
-              </h4>
-            </a>
-            <p className="post-subtitle ">
-              Active Record in Rails provides various methods like exists?,
-              persisted?, destroyed? and many more...
-            </p>
-            <p className="post-meta">November 9, 2021</p>
-          </div>
-          <hr className="my-4"></hr>
-          <div className="post-preview">
-            <a
-              href="https://www.bigbinary.com/blog/rails-7-adds-ability-to-use-predefined-variants"
-              target="_blank"
-              className={styles.post_title_link}
-              rel="noreferrer"
-            >
-              <h4 className="post-title fw-bold">
-                Rails 7 adds the ability to use pre-defined variants
-              </h4>
-            </a>
-            <p className="post-subtitle ">
-              Rails 5.2 introduced ActiveStorage which made it possible to
-              easily upload files to a cloud storage service like Amazon S3,
-              Google Cloud Storage, or Microsoft Azure...
-            </p>
-            <p className="post-meta">October 19, 2021</p>
-          </div>
-          <hr className="my-4"></hr>
-          <div className="post-preview">
-            <a
-              href="https://www.bigbinary.com/blog/rails-7-adds-comparison-validator-to-active-record"
-              target="_blank"
-              className={styles.post_title_link}
-              rel="noreferrer"
-            >
-              <h4 className="post-title fw-bold">
-                Rails 7 adds ComparisonValidator to ActiveRecord
-              </h4>
-            </a>
-            <p className="post-subtitle ">
-              Rails Active Record Validation provides a convenient way to
-              validate the state of an object before it is stored in the
-              database...
-            </p>
-            <p className="post-meta">October 5, 2021</p>
-          </div>
-          <hr className="my-4"></hr>
-          <div className="post-preview">
-            <a
-              href="https://www.bigbinary.com/blog/rails-7-adds-active-record-relation-structurally-compatible"
-              target="_blank"
-              className={styles.post_title_link}
-              rel="noreferrer"
-            >
-              <h4 className="post-title fw-bold">
-                Rails 7 adds ActiveRecord::Relation#structurally_compatible?
-              </h4>
-            </a>
-            <p className="post-subtitle ">
-              ActiveRecord is one of the most powerful features in Rails. With
-              ActiveRecord we can easily query and handle database objects
-              without writing any SQL...
-            </p>
-            <p className="post-meta">September 15, 2021</p>
-          </div>
-          <hr className="my-4"></hr>
-          <div className="d-flex justify-content-end mb-4 mt-4">
-            <a
-              className="text-uppercase text-decoration-none text-muted fw-light"
-              href="#"
-            >
-              Older Posts →
-            </a>
-          </div>
+          {posts.map((post) => {
+            const prettyDate = new Date(post.createdAt).toLocaleString(
+              "en-US",
+              {
+                month: "short",
+                day: "2-digit",
+                year: "numeric",
+              }
+            );
+            return (
+              <article key={post.slug}>
+                <div className="post-preview">
+                  <Link href={post.permalink} passHref>
+                    <a className="post_title_link">
+                      <h4 className="post-title fw-bold">{post.title}</h4>
+                    </a>
+                  </Link>
+                  <p className="post-subtitle">{post.excerpt}</p>
+                  <p className="post-meta">
+                    <time dateTime={post.createdAt}>{prettyDate}</time>
+                  </p>
+                </div>
+                <hr className="my-4"></hr>
+              </article>
+            );
+          })}
+          <OlderPostsLink showOlderPostLink={showOlderPostLink} />
         </div>
       </div>
-      <div></div>
     </div>
   );
 }
