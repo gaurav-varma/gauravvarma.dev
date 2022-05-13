15a372a19151a24e4d9fd802ab405a627ea5aad5
gaurav-varma <gauravvarma.dev@gmail.com>
2022-05-14 00:09:26 +0530

    Remove unused imports.

--- components/codeBlock.js
+++ components/codeBlock.js
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { oneDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
