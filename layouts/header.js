1e30336e4eda9587c0fc10d5da1707cfad9af820
gaurav-varma <gauravvarma.dev@gmail.com>
2022-05-13 16:06:05 +0530

    Wrap Header content inside next/link to redirect it to root path.

--- layouts/header.js
+++ layouts/header.js
@@ -1,20 +1,22 @@
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <header className="d-flex justify-content-center py-3">
-      <div className="py-5 text-center">
-        <img
-          className="d-block mx-auto mb-2 rounded-circle"
-          src="/gauravvarma.jpg"
-          alt="Picture of the author"
-          width={100}
-          height={100}
-          layout="fixed"
-        />
-        <p className={styles.name + ' headshot-text'}>GAURAV VARMA</p>
-      </div>
+      <Link href="/" passHref>
+        <div className="py-5 text-center headshot">
+          <img
+            className="d-block mx-auto mb-2 rounded-circle"
+            src="/gauravvarma.jpg"
+            alt="Picture of the author"
+            width={100}
+            height={100}
+            layout="fixed"
+          />
+          <p className="name headshot-text">GAURAV VARMA</p>
+        </div>
+      </Link>
     </header>
   );
 }
