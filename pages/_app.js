cc0144463d33f6be76c1011db5e2dc2c05862d7c
gaurav-varma <gauravvarma.dev@gmail.com>
2023-05-20 14:43:59 +0530

    Add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "../styles/globals.css";
 import { DefaultSeo } from "next-seo";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -99,6 +100,7 @@ function MyApp({ Component, pageProps }) {
         ]}
       />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
