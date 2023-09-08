b7756678c2966b80ac6f10ec2afa94570afca4af
gaurav-varma <gauravvarma.dev@gmail.com>
2023-09-08 09:42:44 +0530

    Setup eslint and prettier

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    dirs: ['pages', 'components'],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
