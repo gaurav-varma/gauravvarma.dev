0fd8ddf9385ea8c846a2add7051f2bed07b2742e
gaurav-varma <gauravvarma.dev@gmail.com>
2024-09-12 17:13:20 +0530

    Add load more button with logic to load more posts

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import Header from '../../layouts/header';
 import Footer from '../../layouts/footer';
@@ -5,10 +6,27 @@ import BlogList from '../../components/blogList';
 import { getAllPosts } from '../../api/blog';
 
 export default function Blog({ posts }) {
+  const [postsToShow, setPostsToShow] = useState(5);
+
+  const loadMorePosts = () => {
+    setPostsToShow((prevValue) => prevValue + 5);
+  };
+
   return (
     <main>
       <Header />
-      <BlogList posts={posts} />
+      <BlogList posts={posts.slice(0, postsToShow)} />
+      <div className='mt-8 mx-auto justify-center text-center'>
+        {postsToShow < posts.length && (
+          <button
+            onClick={loadMorePosts}
+            type='button'
+            className='btn btn-dark rounded'
+          >
+            Load more
+          </button>
+        )}
+      </div>
       <Footer />
     </main>
   );
