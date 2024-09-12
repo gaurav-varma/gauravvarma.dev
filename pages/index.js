2111412aa7ab573f3f5a0717e1800860e92fc9e3
gaurav-varma <gauravvarma.dev@gmail.com>
2024-09-12 16:53:31 +0530

    Fix linting issues in landing(/) page component

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import Header from '../layouts/header';
 import Footer from '../layouts/footer';
 import BlogList from '../components/blogList';
@@ -8,13 +9,25 @@ export default function Home({ posts }) {
   const recentPosts = posts.slice(0, 5);
   return (
     <main>
-      <Header></Header>
-      <BlogList posts={recentPosts}></BlogList>
+      <Header />
+      <BlogList posts={recentPosts} />
       <OlderPostsLink />
-      <Footer></Footer>
+      <Footer />
     </main>
   );
 }
+
+Home.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      slug: PropTypes.string.isRequired,
+      permalink: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      excerpt: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export function getStaticProps() {
   return {
