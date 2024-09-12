d410206206c4ab177a80e216dff1bbbc83b8537c
gaurav-varma <gauravvarma.dev@gmail.com>
2024-09-12 16:41:54 +0530

    Fix linting issues in /blog page

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../../layouts/header';
 import Footer from '../../layouts/footer';
 import BlogList from '../../components/blogList';
@@ -7,12 +7,24 @@ import { getAllPosts } from '../api/blog';
 export default function Blog({ posts }) {
   return (
     <main>
-      <Header></Header>
+      <Header />
       <BlogList posts={posts} />
-      <Footer></Footer>
+      <Footer />
     </main>
   );
 }
+
+Blog.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      slug: PropTypes.string.isRequired,
+      permalink: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      excerpt: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export function getStaticProps() {
   return {
