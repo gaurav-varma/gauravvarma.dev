288a8a33a36b8a1e9a2b888c0bbbf2163618276b
gaurav-varma <gauravvarma.dev@gmail.com>
2024-09-12 16:31:12 +0530

    Fix linting issues in blog api

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -4,23 +4,27 @@ import matter from 'gray-matter';
 
 const postsDirectory = join(process.cwd(), 'data/blog');
 
-export function getAllPosts() {
-  const filenames = readdirSync(postsDirectory);
-  return filenames
-    .map((filename) => {
-      return getPostBySlug(filename.replace(/\.md$/, ''));
-    })
-    .sort((post1, post2) => (post1.createdAt > post2.createdAt ? -1 : 1));
-}
-
 export function getPostBySlug(slug) {
   const file = readFileSync(
     join(process.cwd(), 'data/blog', `${slug}.md`),
-    'utf8'
+    'utf8',
   );
   const { content, data } = matter(file);
   const body = content.toString();
   const permalink = `/blog/${slug}`;
-  const createdAt = data.createdAt;
-  return { ...data, body, slug, permalink, createdAt };
+  const { createdAt } = data;
+  return {
+    ...data,
+    body,
+    slug,
+    permalink,
+    createdAt,
+  };
+}
+
+export function getAllPosts() {
+  const filenames = readdirSync(postsDirectory);
+  return filenames
+    .map((filename) => getPostBySlug(filename.replace(/\.md$/, '')))
+    .sort((post1, post2) => (post1.createdAt > post2.createdAt ? -1 : 1));
 }
