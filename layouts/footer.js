1963260d3bdbe5f41a5bcccb4fd97e4edce1bcb0
gaurav-varma <gauravvarma.dev@gmail.com>
2024-09-12 16:30:10 +0530

    Fix linting issues in header and footer components

--- layouts/footer.js
+++ layouts/footer.js
@@ -1,13 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="py-3 my-4">
-      <p className="text-center fw-light subtext">
-        The code for this blog is{' '}
+    <footer className='py-3 my-4'>
+      <p className='text-center fw-light subtext'>
+        The code for this blog is
+        {' '}
         <a
-          className="text-decoration-underline fw-light subtext"
-          href="https://github.com/gaurav-varma/gauravvarma.dev"
-          target="_blank"
-          rel="noreferrer"
+          className='text-decoration-underline fw-light subtext'
+          href='https://github.com/gaurav-varma/gauravvarma.dev'
+          target='_blank'
+          rel='noreferrer'
         >
           available on Github
         </a>
