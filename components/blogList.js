71d2d8d66711cd976b4b1732f6ccf26551ad4155
gaurav-varma <gauravvarma.dev@gmail.com>
2024-09-12 15:56:00 +0530

    Fix linting issues in BlogList component

--- components/blogList.js
+++ components/blogList.js
@@ -1,10 +1,11 @@
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 
 export default function BlogList({ posts }) {
   return (
-    <div className="container px-4 px-lg-5">
-      <div className="row gx-4 gx-lg-5 justify-content-center">
-        <div className="col-md-10 col-lg-8 col-xl-8 col-sm-12">
+    <div className='container px-4 px-lg-5'>
+      <div className='row gx-4 gx-lg-5 justify-content-center'>
+        <div className='col-md-10 col-lg-8 col-xl-8 col-sm-12'>
           {posts.map((post) => {
             const prettyDate = new Date(post.createdAt).toLocaleString(
               'en-US',
@@ -12,22 +13,24 @@ export default function BlogList({ posts }) {
                 month: 'short',
                 day: '2-digit',
                 year: 'numeric',
-              }
+              },
             );
             return (
               <article key={post.slug}>
-                <div className="post-preview">
-                  <Link href={post.permalink} passHref>
-                    <a className="post_title_link">
-                      <h4 className="post-title fw-bold">{post.title}</h4>
-                    </a>
+                <div className='post-preview'>
+                  <Link
+                    href={post.permalink}
+                    className='post_title_link'
+                    passHref
+                  >
+                    <h4 className='post-title fw-bold'>{post.title}</h4>
                   </Link>
-                  <p className="post-subtitle">{post.excerpt}</p>
-                  <p className="post-meta">
+                  <p className='post-subtitle'>{post.excerpt}</p>
+                  <p className='post-meta'>
                     <time dateTime={post.createdAt}>{prettyDate}</time>
                   </p>
                 </div>
-                <hr className="my-4"></hr>
+                <hr className='my-4' />
               </article>
             );
           })}
@@ -36,3 +39,15 @@ export default function BlogList({ posts }) {
     </div>
   );
 }
+
+BlogList.propTypes = {
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      slug: PropTypes.string.isRequired,
+      permalink: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      excerpt: PropTypes.string.isRequired,
+      createdAt: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
