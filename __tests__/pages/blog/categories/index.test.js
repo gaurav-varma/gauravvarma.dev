399123507557409f8572fa81ff3144e2b5728c0c
gaurav-varma <gauravvarma.dev@gmail.com>
2026-04-14 13:21:54 +0530

    Show arrow in button only on mobile and improve space around it

--- __tests__/pages/blog/categories/index.test.js
+++ __tests__/pages/blog/categories/index.test.js
@@ -20,7 +20,9 @@ describe('The Categories Page', () => {
     render(<Categories categories={mockCategories} />);
     expect(screen.getByTestId('header')).toBeInTheDocument();
     expect(screen.getByText('All Categories')).toBeInTheDocument();
-    expect(screen.getByText('← View All Blogs')).toBeInTheDocument();
+    expect(
+      screen.getByText('View All Blogs', { exact: false }),
+    ).toBeInTheDocument();
     expect(screen.getByTestId('category-list')).toBeInTheDocument();
     expect(screen.getByTestId('footer')).toBeInTheDocument();
   });
